567b70d4a24b3f21a0f4aa88744c8edef9152df0
Bùi Quang Huy <59251361+huybui76@users.noreply.github.com>
2023-08-09 20:01:12 +0700

    ok

--- features/restaurant/screens/ResScreen.tsx
+++ features/restaurant/screens/ResScreen.tsx
@@ -1,18 +1,47 @@
 import { Text, StyleSheet, View } from 'react-native'
 import React, { Component, SetStateAction } from 'react'
-import { Searchbar } from 'react-native-paper'
+import { ActivityIndicator, Searchbar } from 'react-native-paper'
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ResInfor from '../components/ResInfor';
 import { FlatList } from 'react-native-gesture-handler';
+import { RestaurantsContext } from '../../../services/restaurant/restaurants.context';
+import Colors from '../../../constants/Colors';
+
+type RestaurantType = {
+    // Define the properties of your restaurant object here
+    // For example: id: number, name: string, etc.
+};
+
+type ErrorType = {
+    // Define the properties of your error object here
+    // For example: message: string, code: number, etc.
+};
+
+// Define the type for RestaurantsContext (modify it if needed)
+type RestaurantsContextType = {
+    isLoading: boolean;
+    restaurants: RestaurantType[];
+    error: ErrorType | null;
+};
 
 const ResScreen = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
 
     const onChangeSearch = (query: SetStateAction<string>) => setSearchQuery(query);
 
+    const { isLoading, restaurants, error } = useContext<RestaurantsContextType>(RestaurantsContext);
+
+
+
     return (
         <SafeAreaView style={styles.container}>
+
+            {isLoading && (
+                <View style={styles.Loading}>
+                    <ActivityIndicator style={{ marginLeft: -25 }} size={50} animating={true} color={Colors.light.text} />
+                </View>
+            )}
 
             <View style={styles.searchBar}>
                 <Searchbar
@@ -24,7 +53,7 @@ const ResScreen = () => {
             </View>
             <View style={styles.list}>
                 <FlatList
-                    data={[{ name: 1 }, { name: 2 }, { name: 3 }]}
+                    data={restaurants}
                     renderItem={() => <ResInfor />}
                     keyExtractor={(item) => item.name}
                 //contentContainerStyle={{ paddingVertical: 13 }}
@@ -62,4 +91,14 @@ const styles = StyleSheet.create({
         height: 1,
         width: '80%',
     },
+    Loading: {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+    },
+
 })
+
+function useContext<T>(RestaurantsContext: React.Context<any>): { isLoading: any; restaurants: any; error: any; } {
+    throw new Error('Function not implemented.');
+}
